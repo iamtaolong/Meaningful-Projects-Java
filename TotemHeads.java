1ab27ffd1b7846da0aa8dff5472401ab85753a6b
Tao Long <iamtaolong@Gmail.com>
2019-11-06 16:35:59 -0500

    Add files via upload

---
+++ TotemHeads.java
@@ -0,0 +1,46 @@
+/**************************************************************************************************************
+ * @file TotemHeads.java
+ * @brief This program implements basic knowledge about SOUT.
+ * @author Tao Long
+ * @data January 1.22
+ **************************************************************************************************************/
+
+public class TotemHeads {
+
+
+    public static void main (String[]args) {
+        System.out.println("  |||||||||||||||| ");
+        System.out.println("  |              | ");
+        System.out.println("  |              | ");
+        System.out.println("  |   o      o   | ");
+        System.out.println(" _|              |_");
+        System.out.println("|_                _| ");
+        System.out.println("  |  |________|  | ");
+        System.out.println("  |              | ");
+        System.out.println("  |              | ");
+        System.out.println("  |||||||||||||||| ");
+        System.out.println("  |  __      __  | ");
+        System.out.println("  | !  ! __ !  ! | ");
+        System.out.println("  | !o !/  \\!o ! | ");
+        System.out.println("  | !__!    !__! | ");
+        System.out.println("  |              | ");
+        System.out.println("  |    ///|\\\\\\   | ");
+        System.out.println("   \\            / ");
+        System.out.println("    \\     o    / ");
+        System.out.println("     \\________/ ");
+        System.out.println("    ___|     |___   ");
+        System.out.println("   /    \\   /    \\    ");
+        System.out.println("  /      \\ /      \\    ");
+        System.out.println(" /    __  V   __   \\    ");
+        System.out.println(" \\   /。  \\/ 。  \\  /   ");
+        System.out.println("  \\               /    ");
+        System.out.println("   \\      /_     /    ");
+        System.out.println("    \\           /    ");
+        System.out.println("     \\........./    ");
+        System.out.println("      \\...O.../    ");
+        System.out.println("       \\...../    ");
+        System.out.println("        \\.../    ");
+        System.out.println("         \\./    ");
+        System.out.println("          V   ");
+    }
+}
